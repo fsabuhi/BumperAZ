44493df1a70f268dd6d3dda94b035168144ce582
fsabuhi <93775724+fsabuhi@users.noreply.github.com>
2023-07-15 20:43:44 +0400

    updated functions

--- src/get_all_advertisements_multithreading.py
+++ src/get_all_advertisements_multithreading.py
@@ -3,60 +3,69 @@ import time
 from random import randint
 import json
 import threading
-time_data = []
-while True:
-    start_time = time.time()
-    with open('data.json',"r") as json_file:
-        db = json.load(json_file)
 
-    with open("sent_requests_id.txt", "r") as fp:
+def get_sent_request() -> list:
+    with open("src/sent_requests_id.txt", "r") as fp:
         sent_requests_id = []
         for i in fp:
             x = i[:-1]
             sent_requests_id.append(x)
-        #sent_requests_id.append(list(db.keys()))
-    request_count = 10
-    def get_data():
-        for j in range(request_count):
-            i = randint(1000000,10000000)
-            if i not in sent_requests_id:  #randomize sent requests. it is slower but less obvious
-                start_time = time.time()
-                try:
-                    url = 'https://turbo.az/autos/{}'.format(i)
-                    print(url)
-                    data = get_advertisement_info(url)
-                    db[i]=data
-                    sent_requests_id.append(i)
-                except IndexError:
-                    pass
-    def save():
-        with open("sent_requests_id.txt", "w") as fp:
-            for _id in sent_requests_id:
-                fp.write("{}\n".format(_id))
         fp.close()
-        with open("data.json", "w") as outfile:
-            json.dump(db, outfile)
+        return sent_requests_id
+
+def update_data(new_data:dict):
+    with open("src/data.json", "r" ) as outfile:
+        db = json.loads(outfile.read())
+        db.update(new_data)
         outfile.close()
-    threads=[]
-    thread_count = 10
+    with open("src/data.json", "w" ) as outfile:
+        json.dump(db, outfile,indent = 4)
+        outfile.close()
+
+def update_sent_request(new_ids:list):
+    with open("src/sent_requests_id.txt", "a") as fp:
+        for _id in new_ids:
+            fp.write("{}\n".format(_id)) 
+        fp.close()
+
+def get_data(id):
+    url = "https://turbo.az/autos/{}".format(id)
+    data = get_advertisement_info(url)
+    return data
+
+def main(count):
+    start_time = time.time()
+    data = {}
+    old_sent_requests = get_sent_request()
+    sent_requests = []
+    for i in range(count):
+        id = randint(1000000,9999999)
+        if id not in sent_requests:
+            try:
+                data.update({id : get_data(id)})
+            except:
+                pass
+        sent_requests.append(id)
+    update_sent_request(sent_requests)
+    update_data(data)
+    print((time.time()-start_time)/count)
+
+        
+
+if __name__ == '__main__':
+    threads = []
+    thread_count = 25
+
     for i in range(thread_count):
-        t = threading.Thread(target=get_data)
+        t = threading.Thread(target=main,args=[25])
         t.daemon=True
         threads.append(t)
+
     for i in range(thread_count):
         threads[i].start()
+        time.sleep(1)
     for i in range(thread_count):
         threads[i].join()
-    save()
-    time_spent = time.time()-start_time
-    time_data.append(time_spent)
-    avg = sum(time_data)/len(time_data)
-    print('time left:',((10000000-len(sent_requests_id))/(thread_count*request_count))*avg/60/60,'HOURS')
-    sent_requests_id.append(i)
-    print('DONE!!!')
-
-
-
 
 
 
