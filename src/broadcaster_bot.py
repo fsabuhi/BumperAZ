5c9368368aa42369ce2948ff5fa73f258febda7f
Sabuhi Fatalizada <sabuhif@icloud.com>
2023-08-19 14:03:33 +0400

    Delete bot token for security reasons

--- src/broadcaster_bot.py
+++ src/broadcaster_bot.py
@@ -4,8 +4,8 @@ import json
 import requests
 
 def send_media_group(bot_id,chat_id,mediagroup):
-    print(requests.post('https://api.telegram.org/bot{}/sendMediaGroup'.format('5686031636:AAGlVULyOcEhn3b9l1CUstZbUZFhfTvCbK0'),data={'chat_id': chat_id, 'media': json.dumps(mediagroup)}))
-botid = '5686031636:AAGlVULyOcEhn3b9l1CUstZbUZFhfTvCbK0'
+    print(requests.post('https://api.telegram.org/bot{}/sendMediaGroup'.format(''),data={'chat_id': chat_id, 'media': json.dumps(mediagroup)}))
+botid = ''
 def auto_send(db) -> None: #,autos:list,user:str
     for i in db['users'].keys():
         user_id = i
